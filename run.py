d328b5885248d6a511868939bac217c30733cb6d
StarX <dmytromaloshtan@gmail.com>
2024-02-25 06:14:37 +0900

    Create the scraper2

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from scripts.scraper1 import main as _1         
+# from scripts.scraper1 import main as _1         
+from scripts.scraper2 import main as _2       
 
 
 def run():
-    try: _1() 
-    except Exception as e: print(f'Scraper1: {e}')
+    # try: _1() 
+    # except Exception as e: print(f'Scraper1: {e}')
+    
+    try: _2() 
+    except Exception as e: print(f'Scraper2: {e}')
     
     
 if __name__ == "__main__":
