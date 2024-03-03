068d9d2b3aaa080a8acf37804c6f97f30112c426
StarX <dmytromaloshtan@gmail.com>
2024-03-03 23:05:25 +0900

    Create the scraper34

---
+++ scripts/scraper34.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 34
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "a.content-item-link")
+
+    data = []
+
+    for item in items:
+        link = item.get_attribute("href").strip()
+        title = item.find_element(
+            By.CSS_SELECTOR, "h4.content-item-heading-text"
+        ).text.strip()
+
+        data.append(
+            [
+                title,
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
