386607a08e58e5bf3d23e336aaf0a0352a7cceba
StarX <dmytromaloshtan@gmail.com>
2024-03-08 06:27:50 +0900

    Create the scraper56

---
+++ scripts/scraper56.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 56
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(
+            By.CSS_SELECTOR,
+            "button#onetrust-accept-btn-handler",
+        ).click()
+    except Exception as e:
+        print(f"Scraper{key} cookiee button: {e}")
+
+    time.sleep(2)
+
+    driver.find_element(
+        By.CSS_SELECTOR,
+        "button[aria-controls='CountryBody']",
+    ).click()
+
+    time.sleep(2)
+
+    driver.find_element(By.XPATH, "//span[contains(text(), 'United Kingdom')]").click()
+
+    time.sleep(2)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "li.jobs-list-item")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "div.job-title").text.strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
