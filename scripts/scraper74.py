0a3ec832e2328dd67f4c9402f9a45f6fe296360e
StarX <dmytromaloshtan@gmail.com>
2024-03-11 23:29:44 +0900

    Create the scraper 74 !

---
+++ scripts/scraper74.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 74
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    # try:
+    #     driver.find_element(
+    #         By.CSS_SELECTOR,
+    #         "button#js-cookie-accept",
+    #     ).click()
+    # except Exception as e:
+    #     print(f"Scraper{key} cookiee button: {e}")
+
+    # time.sleep(4)
+
+    data = []
+
+    # items = driver.find_elements(By.CSS_SELECTOR, "div.card-job")
+
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+    #     location = item.find_element(By.CSS_SELECTOR, "p").text.strip()
+
+    #     for str in [
+    #         "London",
+    #         "New York",
+    #         "UK",
+    #         "US",
+    #     ]:
+    #         if str in location:
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+    #                     com,
+    #                     location,
+    #                     link,
+    #                 ]
+    #             )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
