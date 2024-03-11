941d2bcc3c399e8943c6d2cd760fa609900349ce
StarX <dmytromaloshtan@gmail.com>
2024-03-11 22:10:31 +0900

    Create the scraper 69

---
+++ scripts/scraper69.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 69
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    
+    time.sleep(4)
+
+    data = []
+
+    items = driver.find_elements(By.CSS_SELECTOR, "tr.base-job")
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "td").text.strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
