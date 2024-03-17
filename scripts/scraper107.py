52f11b02889ee1be6f091a600093d4fd9795e61a
StarX <dmytromaloshtan@gmail.com>
2024-03-18 08:42:54 +0900

    Create the scraper 107

---
+++ scripts/scraper107.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 107
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    try:
+        driver.find_element(
+            By.CSS_SELECTOR, "button#onetrust-accept-btn-handler"
+        ).click()
+    except Exception as e:
+        print(f"Scraper{key} cookiee button: {e}")
+
+    time.sleep(4)
+
+    flag = True
+    data = []
+
+    while flag:
+        try:
+            time.sleep(4)
+
+            items = driver.find_elements(
+                By.CSS_SELECTOR, "a.JobListings_listing__qqquK"
+            )
+
+            for item in items:
+                link = item.get_attribute("href").strip()
+                location = item.find_element(
+                    By.CSS_SELECTOR, 'h4[data-testid="JobListings-location"]'
+                ).text.strip()
+
+                if location in ["United Kingdom", "United States"]:
+                    data.append(
+                        [
+                            item.find_element(
+                                By.CSS_SELECTOR, 'h3[data-testid="JobListings-title"]'
+                            ).text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+
+            nextBtn = driver.find_element(By.CSS_SELECTOR, 'button[aria-label="next"]')
+
+            if nextBtn.is_enabled():
+                nextBtn.click()
+            else:
+                flag = False
+        except:
+            flag = False
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
