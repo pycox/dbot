b24b7a474e1c064b1a9e472c9c11feffcac1616b
StarX <dmytromaloshtan@gmail.com>
2024-03-20 00:36:30 +0900

    Create the scraper 114

---
+++ scripts/scraper114.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 114
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul > div")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                com,
+                item.find_element(By.CSS_SELECTOR, "p.jss-f72").text.strip(),
+                link,
+            ]
+        )
+
+    driver.quit()
+    
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
