d700703f7343760883f4848d072f452a84b60f98
StarX <dmytromaloshtan@gmail.com>
2024-03-21 18:29:25 +0900

    Create the scraper 121 !

---
+++ scripts/scraper121.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 121
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    # items = driver.find_element(By.CSS_SELECTOR, "ul").find_elements(
+    #     By.CSS_SELECTOR, "li"
+    # )
+
+    data = []
+
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+
+    #     data.append(
+    #         [
+    #             item.find_element(By.CSS_SELECTOR, "h2").text.strip(),
+    #             com,
+    #             "UK",
+    #             link,
+    #         ]
+    #     )
+
+    driver.quit()
+    
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
