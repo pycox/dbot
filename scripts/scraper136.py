49bb8ba79c5b777a697351482c907a6e54bee8b6
StarX <dmytromaloshtan@gmail.com>
2024-03-26 06:21:49 +0900

    Create the scraper 136

---
+++ scripts/scraper136.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 136
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    #
+    time.sleep(4)
+
+    try:
+        driver.find_element(
+            By.CSS_SELECTOR, "button#onetrust-accept-btn-handler"
+        ).click()
+    except Exception as e:
+        print(f"Scraper{key} cookie Button: {e}")
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "div.greenhouse-job-listing")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.get_attribute("data-sort-path").strip()
+
+        for str in [
+            "London",
+            "New York",
+            "San Francisco",
+            "United States",
+            "United Kingdom",
+        ]:
+            if str in location:
+
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
