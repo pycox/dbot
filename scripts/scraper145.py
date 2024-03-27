2cbf8719f0e74c3fc453e455be3a66256cd9f53f
brave <bravelion0507@gmail.com>
2024-03-28 00:11:23 +0800

    Create the scraper 145 !

---
+++ scripts/scraper145.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 145
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+
+if __name__ == "__main__":
+    main()
