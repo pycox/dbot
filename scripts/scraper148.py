1cba7c0a76dc1f3fdf697a2b2c98d1bcd84cf92c
brave <bravelion0507@gmail.com>
2024-03-28 22:21:25 +0800

    Create the scraper 148 #

---
+++ scripts/scraper148.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 148
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+
+
+
+if __name__ == "__main__":
+    main()
+    
