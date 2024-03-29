a8c299e613fe9ec8652bc627bc377afb8454b541
StarX <dmytromaloshtan@gmail.com>
2024-03-30 01:20:40 +0900

    Create the scraper 170

---
+++ scripts/scraper170.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 170
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    # time.sleep(4)
+
+    # try:
+    #     driver.find_element(
+    #         By.CSS_SELECTOR,
+    #         "button[data-action='click->common--cookies--alert#acceptAll']",
+    #     ).click()
+    # except Exception as e:
+    #     print(f"Scraper{key} cookie Button: {e}")
+
+    time.sleep(4)
+
+    dom = driver.find_element(By.CSS_SELECTOR, "ul#jobs_list_container")
+
+    items = dom.find_elements(By.CSS_SELECTOR, "li")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "span").text.strip(),
+                com,
+                "UK",
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
