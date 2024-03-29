879c6f60c484fa7c0831af1a493c2617cd0039c1
StarX <dmytromaloshtan@gmail.com>
2024-03-30 00:38:20 +0900

    Create the scraper 167

---
+++ scripts/scraper167.py
@@ -0,0 +1,70 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 167
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    flag = True
+    data = []
+
+    while flag:
+        try:
+            time.sleep(4)
+
+            items = driver.find_elements(By.CSS_SELECTOR, "div.rowContainerHolder")
+
+            for item in items:
+                link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+                location = item.find_element(
+                    By.CSS_SELECTOR, "span.vacancyColumn"
+                ).text.strip()
+
+                for str in [
+                    "London",
+                    "New York",
+                    "San Francisco",
+                    "United States",
+                    "United Kingdom",
+                ]:
+                    if str in location:
+
+                        data.append(
+                            [
+                                item.find_element(
+                                    By.CSS_SELECTOR, "div.rowHeader"
+                                ).text.strip(),
+                                com,
+                                location,
+                                link,
+                            ]
+                        )
+
+                        break
+
+            nextBtn = driver.find_element(By.CSS_SELECTOR, "a.scroller_movenext")
+
+            if nextBtn.get_attribute("disabled") == "true":
+
+                flag = False
+                break
+            else:
+                nextBtn.click()
+        except:
+            flag = False
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
