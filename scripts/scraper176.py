4cc51c167de79f10bedd591f5ae3a8b3d2180b09
thecoderparth <parthpatel4042@gmail.com>
2024-04-03 21:39:21 +0530

    Created the scraper 176

---
+++ scripts/scraper176.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 174
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul#jobs_list_container > li.w-full")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'a > div > span:last-child').text.strip()
+        
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "a > span").text.strip(),
+                com,
+                location,
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
