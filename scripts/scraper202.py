687922d073b2b1621b8240588c7f3d8ac6d4d8a6
thecoderparth <parthpatel4042@gmail.com>
2024-04-04 14:11:37 +0530

    Created the scraper 202

---
+++ scripts/scraper202.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 202
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "div.job-vacancy")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "div.job-vacancy__apply > a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'div.job-vacancy__table > div:nth-child(3)').text.strip()
+        location = location.replace("Location:", "").strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "div.job-vacancy__title").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
