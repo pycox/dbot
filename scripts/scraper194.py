c2c2accba06db17d53db9afa1556657638494550
thecoderparth <parthpatel4042@gmail.com>
2024-04-04 14:06:20 +0530

    Created the scraper 194

---
+++ scripts/scraper194.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 194
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "a.card.jobs-item")
+
+    data = []
+
+    for item in items:
+        link = item.get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'div.card-header > ul > li[title="Location"]').text.strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "div.card-header > h2").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
