4533b4ab1bd1421450bf0e1a1223d41e2f7ceeb8
thecoderparth <parthpatel4042@gmail.com>
2024-04-09 23:37:41 +0530

    Created the scraper 214

---
+++ scripts/scraper214.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 214
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    data = []
+    flag = True
+    while flag:
+      try:
+        button = driver.find_element(By.CSS_SELECTOR, 'div.careers-search__load-more > button').click()
+        time.sleep(4)
+      except:
+        flag = False
+        print("No more load button")
+
+    items = driver.find_elements(By.CSS_SELECTOR, "li.careers-search__list-item")
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'p').text.strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
