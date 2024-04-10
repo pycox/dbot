ac3dafa632405d5fa2f40aa4fb861dd8b8bd79ed
thecoderparth <parthpatel4042@gmail.com>
2024-04-10 18:10:36 +0530

    Created the scraper 225

---
+++ scripts/scraper225.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 225
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "div.v2_m_small.w-dyn-item")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a.v2_text_link.w-condition-invisible").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'div.v2_card--content--subheading.v2_light_gray').text.strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h4").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
