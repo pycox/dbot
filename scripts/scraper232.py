7a38051a607eaef7eed0e8c037cf1a45dbaa460d
thecoderparth <parthpatel4042@gmail.com>
2024-04-10 19:28:14 +0530

    Created the scraper 232

---
+++ scripts/scraper232.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 232
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    flag = True
+    while flag:
+      try:
+        driver.find_element(By.CSS_SELECTOR, "button[data-ui='load-more-button']").click()
+        time.sleep(4)
+      except:
+        flag = False
+        print("No More Jobs")
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul > li[role='listitem']")
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'span[data-ui="job-location"]').text.strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3 > span").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
