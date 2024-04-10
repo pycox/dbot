0b63c1e5ac7ad09f781f9d4e7743df7550aea65e
thecoderparth <parthpatel4042@gmail.com>
2024-04-10 18:13:12 +0530

    Created the scraper 230!

---
+++ scripts/scraper230.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 230
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
+
+    data = []
+
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+
+    #     data.append(
+    #         [
+    #             item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
+    #             com,
+    #             location,
+    #             link,
+    #         ]
+    #     )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
