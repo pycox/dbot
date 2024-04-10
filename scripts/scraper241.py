eea1971fc8904d11460d4c92a89df48588f9de9b
thecoderparth <parthpatel4042@gmail.com>
2024-04-10 22:25:58 +0530

    Created the scraper 241

---
+++ scripts/scraper241.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 241
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    flag = True
+    data = []
+
+    while flag:
+        items = driver.find_elements(By.CSS_SELECTOR, "li.css-1q2dra3")
+        for item in items:
+            link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+            location = item.find_element(By.CSS_SELECTOR, 'dd.css-129m7dg').text.strip()
+
+            for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+                if (str in location):
+                    data.append(
+                        [
+                            item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                            com,
+                            location,
+                            link,
+                        ]
+                    )
+                    break
+
+        try:
+          driver.find_element(By.CSS_SELECTOR, "button[data-uxi-widget-type='stepToNextButton']").click()
+          time.sleep(4)
+        except:
+          flag = False
+          print("No More Jobs")
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
