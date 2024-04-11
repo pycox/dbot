31582d47598ae1b0db2dfdb6eee2e70d4dce8a3e
thecoderparth <parthpatel4042@gmail.com>
2024-04-11 15:30:18 +0530

    Created the scraper 247

--- scripts/scraper247.py
+++ scripts/scraper247.py
@@ -1,14 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-# from utils import readUrl, updateDB
+from utils import readUrl, updateDB
 import time
 
 
 def main():
     key = 247
-    com, url = "", "https://www.bjss.com/careers/search"
-        # com, url = readUrl(key)
+    com, url = readUrl(key)
     options = Options()
     options.add_argument("--log-level=3")
     driver = webdriver.Chrome(options=options)
@@ -19,12 +18,10 @@ def main():
 
     while flag:
         time.sleep(4)
-        items = driver.find_elements(By.CSS_SELECTOR, "div.border.p-6.md:p-8.md:flex.md:flex-row.justify-between.items-center.featured-positions-position.relative")
+        items = driver.find_elements(By.CSS_SELECTOR, "div.page-center.w-full.featured-positions.flex.flex-col.gap-y-4 > div")
         for item in items:
             link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
             location = item.find_element(By.CSS_SELECTOR, 'div').text.strip()
-
-            print(location)
 
             for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
                 if (str in location):
@@ -47,8 +44,8 @@ def main():
 
 
     driver.quit()
-    print(data)
-    # updateDB(key, data)
+
+    updateDB(key, data)
 
 
 if __name__ == "__main__":
