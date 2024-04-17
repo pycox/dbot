340fdfa8b0ef68a3ec782e15402de5a58fcbb6ec
thecoderparth <parthpatel4042@gmail.com>
2024-04-17 18:38:43 +0530

    Created the scraper 255

---
+++ scripts/scraper255.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 255
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    items = driver.find_elements(By.CSS_SELECTOR, "ul.whr-items > li")
+
+    data = []
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'li.whr-location').text.strip()
+
+        data.append(
+            [
+                item.find_element(By.CSS_SELECTOR, "a").text.strip(),
+                com,
+                location,
+                link,
+            ]
+        )
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
