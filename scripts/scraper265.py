70d8e2f57fd8dfce93c9d0cefabc462cf77b7a28
thecoderparth <parthpatel4042@gmail.com>
2024-04-17 18:42:26 +0530

    Created the scraper 265!

---
+++ scripts/scraper265.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 265
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    time.sleep(4)
+
+    # items = driver.find_elements(By.CSS_SELECTOR, "mat-expansion-panel")
+
+    data = []
+
+    # for item in items:
+    #     link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+    #     location = item.find_element(By.CSS_SELECTOR, 'span.label-value.location').text.strip()
+
+    #     for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+    #         if (str in location):
+    #             data.append(
+    #                 [
+    #                     item.find_element(By.CSS_SELECTOR, "span[itemprop='title']").text.strip(),
+    #                     com,
+    #                     location,
+    #                     link,
+    #                 ]
+    #             )
+    #             break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
