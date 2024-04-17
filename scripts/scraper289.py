e75d21f8654f8c7f4a214869aa232f8cc2ffb2c1
thecoderparth <parthpatel4042@gmail.com>
2024-04-17 18:56:27 +0530

    Created the scraper 289

---
+++ scripts/scraper289.py
@@ -0,0 +1,51 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from utils import readUrl, updateDB
+import time
+
+
+def main():
+    key = 289
+    com, url = readUrl(key)
+    options = Options()
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+
+    data = []
+    flag = True
+    while flag:
+      time.sleep(4)
+      try:
+        driver.find_element(By.CSS_SELECTOR, "a.load_more_jobs").click()
+      except:
+        flag = False
+        print("No More Jobs")
+
+    time.sleep(4)
+    items = driver.find_elements(By.CSS_SELECTOR, "ul.job_listings > li")
+
+    for item in items:
+        link = item.find_element(By.CSS_SELECTOR, "a").get_attribute("href").strip()
+        location = item.find_element(By.CSS_SELECTOR, 'a.google_map_link').text.strip()
+
+        for str in ['London', 'New York', 'San Francisco', 'United States', 'United Kingdom', 'UK', 'USA', 'US']:
+            if (str in location):
+                data.append(
+                    [
+                        item.find_element(By.CSS_SELECTOR, "h3 > a").text.strip(),
+                        com,
+                        location,
+                        link,
+                    ]
+                )
+                break
+
+    driver.quit()
+
+    updateDB(key, data)
+
+
+if __name__ == "__main__":
+    main()
